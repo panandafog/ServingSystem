bd3e843251daac468ccd6e8bb04737906d3a56c6
panandafog <panandafog@gmail.com>
2020-09-20 12:47:27 +0300

    Initial Commit

---
+++ ServingSystem/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ServingSystem
+//
+//  Created by panandafog on 20.09.2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
